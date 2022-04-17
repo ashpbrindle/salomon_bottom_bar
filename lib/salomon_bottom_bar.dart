1df6bbd6d7494740d2196e80900434058c800dc2
Ashley Brindle <59151951+ashpbrindle@users.noreply.github.com>
2022-04-17 17:37:50 +0100

    Update salomon_bottom_bar.dart

--- lib/salomon_bottom_bar.dart
+++ lib/salomon_bottom_bar.dart
@@ -12,6 +12,7 @@ class SalomonBottomBar extends StatelessWidget {
     this.selectedItemColor,
     this.unselectedItemColor,
     this.selectedColorOpacity,
+    this.backgroundColour,
     this.itemShape = const StadiumBorder(),
     this.margin = const EdgeInsets.all(8),
     this.itemPadding = const EdgeInsets.symmetric(vertical: 10, horizontal: 16),
@@ -30,6 +31,8 @@ class SalomonBottomBar extends StatelessWidget {
 
   /// The color of the icon and text when the item is selected.
   final Color? selectedItemColor;
+
+  final backgroundColour;
 
   /// The color of the icon and text when the item is not selected.
   final Color? unselectedItemColor;
@@ -58,100 +61,103 @@ class SalomonBottomBar extends StatelessWidget {
 
     return SafeArea(
       minimum: margin,
-      child: Row(
-        /// Using a different alignment when there are 2 items or less
-        /// so it behaves the same as BottomNavigationBar.
-        mainAxisAlignment: items.length <= 2
-            ? MainAxisAlignment.spaceEvenly
-            : MainAxisAlignment.spaceBetween,
-        children: [
-          for (final item in items)
-            TweenAnimationBuilder<double>(
-              tween: Tween(
-                end: items.indexOf(item) == currentIndex ? 1.0 : 0.0,
-              ),
-              curve: curve,
-              duration: duration,
-              builder: (context, t, _) {
-                final _selectedColor = item.selectedColor ??
-                    selectedItemColor ??
-                    theme.primaryColor;
+      child: Container(
+        color: backgroundColour,
+        child: Row(
+          /// Using a different alignment when there are 2 items or less
+          /// so it behaves the same as BottomNavigationBar.
+          mainAxisAlignment: items.length <= 2
+              ? MainAxisAlignment.spaceEvenly
+              : MainAxisAlignment.spaceBetween,
+          children: [
+            for (final item in items)
+              TweenAnimationBuilder<double>(
+                tween: Tween(
+                  end: items.indexOf(item) == currentIndex ? 1.0 : 0.0,
+                ),
+                curve: curve,
+                duration: duration,
+                builder: (context, t, _) {
+                  final _selectedColor = item.selectedColor ??
+                      selectedItemColor ??
+                      theme.primaryColor;
 
-                final _unselectedColor = item.unselectedColor ??
-                    unselectedItemColor ??
-                    theme.iconTheme.color;
+                  final _unselectedColor = item.unselectedColor ??
+                      unselectedItemColor ??
+                      theme.iconTheme.color;
 
-                return Material(
-                  color: Color.lerp(
-                      _selectedColor.withOpacity(0.0),
-                      _selectedColor.withOpacity(selectedColorOpacity ?? 0.1),
-                      t),
-                  shape: itemShape,
-                  child: InkWell(
-                    onTap: () => onTap?.call(items.indexOf(item)),
-                    customBorder: itemShape,
-                    focusColor: _selectedColor.withOpacity(0.1),
-                    highlightColor: _selectedColor.withOpacity(0.1),
-                    splashColor: _selectedColor.withOpacity(0.1),
-                    hoverColor: _selectedColor.withOpacity(0.1),
-                    child: Padding(
-                      padding: itemPadding -
-                          (Directionality.of(context) == TextDirection.ltr
-                              ? EdgeInsets.only(right: itemPadding.right * t)
-                              : EdgeInsets.only(left: itemPadding.left * t)),
-                      child: Row(
-                        children: [
-                          IconTheme(
-                            data: IconThemeData(
-                              color: Color.lerp(
-                                  _unselectedColor, _selectedColor, t),
-                              size: 24,
+                  return Material(
+                    color: Color.lerp(
+                        _selectedColor.withOpacity(0.0),
+                        _selectedColor.withOpacity(selectedColorOpacity ?? 0.1),
+                        t),
+                    shape: itemShape,
+                    child: InkWell(
+                      onTap: () => onTap?.call(items.indexOf(item)),
+                      customBorder: itemShape,
+                      focusColor: _selectedColor.withOpacity(0.1),
+                      highlightColor: _selectedColor.withOpacity(0.1),
+                      splashColor: _selectedColor.withOpacity(0.1),
+                      hoverColor: _selectedColor.withOpacity(0.1),
+                      child: Padding(
+                        padding: itemPadding -
+                            (Directionality.of(context) == TextDirection.ltr
+                                ? EdgeInsets.only(right: itemPadding.right * t)
+                                : EdgeInsets.only(left: itemPadding.left * t)),
+                        child: Row(
+                          children: [
+                            IconTheme(
+                              data: IconThemeData(
+                                color: Color.lerp(
+                                    _unselectedColor, _selectedColor, t),
+                                size: 24,
+                              ),
+                              child: items.indexOf(item) == currentIndex
+                                  ? item.activeIcon ?? item.icon
+                                  : item.icon,
                             ),
-                            child: items.indexOf(item) == currentIndex
-                                ? item.activeIcon ?? item.icon
-                                : item.icon,
-                          ),
-                          ClipRect(
-                            child: SizedBox(
-                              /// TODO: Constrain item height without a fixed value
-                              ///
-                              /// The Align property appears to make these full height, would be
-                              /// best to find a way to make it respond only to padding.
-                              height: 20,
-                              child: Align(
-                                alignment: Alignment(-0.2, 0.0),
-                                widthFactor: t,
-                                child: Padding(
-                                  padding: Directionality.of(context) ==
-                                          TextDirection.ltr
-                                      ? EdgeInsets.only(
-                                          left: itemPadding.left / 2,
-                                          right: itemPadding.right)
-                                      : EdgeInsets.only(
-                                          left: itemPadding.left,
-                                          right: itemPadding.right / 2),
-                                  child: DefaultTextStyle(
-                                    style: TextStyle(
-                                      color: Color.lerp(
-                                          _selectedColor.withOpacity(0.0),
-                                          _selectedColor,
-                                          t),
-                                      fontWeight: FontWeight.w600,
+                            ClipRect(
+                              child: SizedBox(
+                                /// TODO: Constrain item height without a fixed value
+                                ///
+                                /// The Align property appears to make these full height, would be
+                                /// best to find a way to make it respond only to padding.
+                                height: 20,
+                                child: Align(
+                                  alignment: Alignment(-0.2, 0.0),
+                                  widthFactor: t,
+                                  child: Padding(
+                                    padding: Directionality.of(context) ==
+                                            TextDirection.ltr
+                                        ? EdgeInsets.only(
+                                            left: itemPadding.left / 2,
+                                            right: itemPadding.right)
+                                        : EdgeInsets.only(
+                                            left: itemPadding.left,
+                                            right: itemPadding.right / 2),
+                                    child: DefaultTextStyle(
+                                      style: TextStyle(
+                                        color: Color.lerp(
+                                            _selectedColor.withOpacity(0.0),
+                                            _selectedColor,
+                                            t),
+                                        fontWeight: FontWeight.w600,
+                                      ),
+                                      child: item.title,
                                     ),
-                                    child: item.title,
                                   ),
                                 ),
                               ),
                             ),
-                          ),
-                        ],
+                          ],
+                        ),
                       ),
                     ),
-                  ),
-                );
-              },
-            ),
-        ],
+                  );
+                },
+              ),
+          ],
+        ),
       ),
     );
   }
