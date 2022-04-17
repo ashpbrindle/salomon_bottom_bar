9b3f3a78355c7d6657e5bd003bae5d55b3706ad0
Ashley Brindle <59151951+ashpbrindle@users.noreply.github.com>
2022-04-17 19:33:35 +0100

    Update salomon_bottom_bar.dart

--- lib/salomon_bottom_bar.dart
+++ lib/salomon_bottom_bar.dart
@@ -114,7 +114,7 @@ class SalomonBottomBar extends StatelessWidget {
                               /// best to find a way to make it respond only to padding.
                               height: 20,
                               child: Align(
-                                alignment: Alignment(-0.2, 0.0),
+                                alignment: Alignment.center,
                                 widthFactor: t,
                                 child: Padding(
                                   padding: Directionality.of(context) ==
